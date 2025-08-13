53d9249172107612eb384c31ce80b9a9ed4f52e6
Anoshafatima8 <sp23-bcs-133@cuilahore.edu.pk>
2025-08-13 13:04:06 +0500

    Final changes

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ from datetime import datetime
 # Prints a friendly hello with today's local date (YYYY-MM-DD)
 today = datetime.now().strftime("%Y-%m-%d")
 print(f"Hello, World! Today is {today}.")
+print ("THIS IS UPDATED CODE")
+print("shit commit to kia nai hai")
